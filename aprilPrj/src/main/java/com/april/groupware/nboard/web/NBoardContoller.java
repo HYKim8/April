23954dadd36a720cd0517d9816d11a6e3dd8fd4b
sist <sist@DESKTOP-IIRC78O>
2020-05-18 17:56:39 +0900

    전사게시판 Controller,Test_양은영_20200518

--- aprilPrj/src/main/java/com/april/groupware/nboard/web/NBoardContoller.java
+++ aprilPrj/src/main/java/com/april/groupware/nboard/web/NBoardContoller.java
@@ -1,5 +1,6 @@
 package com.april.groupware.nboard.web;
 
+import java.util.List;
 import java.util.Locale;
 
 import org.slf4j.Logger;
@@ -7,11 +8,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.april.groupware.cmn.MessageVO;
+import com.april.groupware.cmn.SearchVO;
+import com.april.groupware.cmn.StringUtil;
+import com.april.groupware.code.service.CodeService;
+import com.april.groupware.code.service.CodeVO;
 import com.april.groupware.nboard.service.NBoardService;
 import com.april.groupware.nboard.service.NBoardVO;
 import com.google.gson.Gson;
@@ -22,11 +28,166 @@ public class NBoardContoller {
 Logger  LOG = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
-	NBoardService nboardService;
+	NBoardService boardService;
+	
+	@Autowired
+	CodeService codeService;
 	
 	@Autowired
 	MessageSource messageSource;
 	
+	//목록조회
+	@RequestMapping(value = "nboard/do_retrieve.do",method = RequestMethod.GET)
+	public String doRetrieve(SearchVO  search,Model model) {
+		//param 기본값 처리
+		if(search.getPageNum()==0) {
+			search.setPageNum(1);
+		}
+		
+		if(search.getPageSize()==0) {
+			search.setPageSize(10);
+		}
+		
+		
+		search.setSearchDiv(StringUtil.nvl(search.getSearchDiv()));
+		search.setSearchWord(StringUtil.nvl(search.getSearchWord().trim()));
+		
+		LOG.debug("1=================");
+		LOG.debug("1=param="+search);
+		LOG.debug("1=================");
+		//검색조건 화면으로 전달.
+		model.addAttribute("vo", search);
+		//TODO: codeTable : 검색조건,페이지 사이즈
+		//검색조건
+		CodeVO  code=new CodeVO();
+		code.setCodeTypeId("BOARD_SEARCH");
+		List<CodeVO> searchList=(List<CodeVO>) this.codeService.doRetrieve(code);
+		LOG.debug("1.1=searchList="+searchList);
+		model.addAttribute("searchList", searchList);
+		
+		//페이지 사이즈: PAGE_SIZE
+		code.setCodeTypeId("PAGE_SIZE");
+		List<CodeVO> pageSizeList=(List<CodeVO>) this.codeService.doRetrieve(code);
+		LOG.debug("1.1=pageSizeList="+pageSizeList);
+		model.addAttribute("pageSizeList", pageSizeList);
+		
+		
+		List<NBoardVO> list = (List<NBoardVO>) this.boardService.doRetrieve(search);
+		//조회결과 화면 전달
+		model.addAttribute("list", list);
+		for(NBoardVO vo:list) {
+			LOG.debug("1.1=out="+vo);
+		}
+		
+		//총건수
+		int totalCnt = 0;
+		if(null !=list &&  list.size()>0) {
+			totalCnt = list.get(0).getTotalCnt();
+		}
+		
+		LOG.debug("1.2=================");
+		LOG.debug("1.2=totalCnt="+totalCnt);
+		LOG.debug("1.2=================");
+		//조회결과 화면 전달
+		model.addAttribute("totalCnt", totalCnt);
+		return "board/board_list";// "/board/board_list.jsp
+	}
+	
+	//수정
+	@RequestMapping(value = "nboard/do_update.do",method = RequestMethod.POST
+			,produces = "application/json; charset=UTF-8")
+	@ResponseBody
+	public String doUpdate(NBoardVO  board,Locale locale) {
+		LOG.debug("1=================");
+		LOG.debug("1=param="+board);
+		LOG.debug("1=================");
+				
+		if( 0 == board.getNbNo()) {		
+
+			throw new IllegalArgumentException("nbNo를 확인 하세요.");			
+		}
+		
+		int flag = this.boardService.doUpdate(board);
+		
+		MessageVO message=new MessageVO();
+		
+		if(flag>0) {
+			message.setMsgId(String.valueOf(flag));
+			message.setMsgMsg("수정 성공.");
+		}else {
+			message.setMsgId(String.valueOf(flag));
+			message.setMsgMsg("수정 실패.");			
+		}
+		
+		Gson gson=new Gson();
+		String jsonStr = gson.toJson(message);
+		LOG.debug("1.2=================");
+		LOG.debug("1.2=jsonStr="+jsonStr);
+		LOG.debug("1.2=================");		
+		return jsonStr;
+	}
+	
+	//삭제
+	@RequestMapping(value = "nboard/do_delete.do",method = RequestMethod.POST
+			,produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public String doDelete(NBoardVO  board,Locale locale) {
+		//param board_id
+		LOG.debug("1=================");
+		LOG.debug("1=param="+board);
+		LOG.debug("1=================");
+		
+		if( 0 == board.getNbNo()) {		
+
+			throw new IllegalArgumentException("nbNo를 확인 하세요.");			
+		}		
+		
+		int flag = this.boardService.doDelete(board);
+		MessageVO message=new MessageVO();
+		//삭제 성공
+		if(flag>0) {
+			message.setMsgId(String.valueOf(flag));
+			message.setMsgMsg("삭제 되었습니다.");
+		//실패	
+		}else {
+			message.setMsgId(String.valueOf(flag));
+			message.setMsgMsg("삭제 실패.");			
+		}
+		
+		Gson gson=new Gson();
+		String gsonStr = gson.toJson(message);
+		
+		LOG.debug("1=================");
+		LOG.debug("1=gsonStr="+gsonStr);
+		LOG.debug("1=================");	
+		
+		
+		return gsonStr;
+	}
+	
+	//단건조회
+	@RequestMapping(value = "nboard/do_selectone.do",method = RequestMethod.GET)
+	public String doSelectOne(NBoardVO  board,Locale locale,Model model) {
+		//param NbNo
+		LOG.debug("1=================");
+		LOG.debug("1=param="+board);
+		LOG.debug("1=================");
+		
+		if( 0 == board.getNbNo()) {		
+
+			throw new IllegalArgumentException("nbNo를 확인 하세요.");			
+		}
+		
+		NBoardVO  outVO =(NBoardVO) this.boardService.doSelectOne(board);
+		LOG.debug("1.1.=================");
+		LOG.debug("1.1=outVO="+outVO);
+		LOG.debug("1.1=================");	
+		model.addAttribute("vo", outVO);
+		
+		return "views/nboard_read"; // views/nboard_read.jsp
+	}
+	
+	//등록
 	@RequestMapping(value = "nboard/do_insert.do",method = RequestMethod.POST
 			,produces = "application/json; charset=UTF-8")
 	@ResponseBody
@@ -35,7 +196,7 @@ Logger  LOG = LoggerFactory.getLogger(this.getClass());
 		LOG.debug("1=param="+nboard);
 		LOG.debug("1=================");
 		
-		int flag = this.nboardService.doInsert(nboard);
+		int flag = this.boardService.doInsert(nboard);
 		
 		MessageVO message=new MessageVO();
 		
--- aprilPrj/src/test/java/com/april/groupware/nboard/TestNBoardController.java
+++ aprilPrj/src/test/java/com/april/groupware/nboard/TestNBoardController.java
@@ -1,17 +1,15 @@
 package com.april.groupware.nboard;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -19,8 +17,6 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -31,15 +27,11 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.april.groupware.nboard.service.NBoardService;
 import com.april.groupware.nboard.service.NBoardVO;
 import com.april.groupware.nboard.service.imple.NBoardDaoImple;
-import com.google.gson.Gson;
 
 
 @WebAppConfiguration
@@ -49,7 +41,7 @@ import com.google.gson.Gson;
                                    })
 public class TestNBoardController {
 
-	private final Logger  LOG = LoggerFactory.getLogger(TestNBoardController.class);
+private final Logger  LOG = LoggerFactory.getLogger(TestNBoardController.class);
 	
 	@Autowired
 	WebApplicationContext  webApplicationContext;
@@ -57,12 +49,11 @@ public class TestNBoardController {
 	private List<NBoardVO> boardList;
 	
 	@Autowired
-	NBoardService  boardService;
+	NBoardService boardService;
 	
 	@Autowired
-	NBoardDaoImple  dao;
+	NBoardDaoImple dao;
 	
-	 
 	//브라우저 대신 Mock
 	private MockMvc mockMvc;
 	
@@ -72,13 +63,11 @@ public class TestNBoardController {
 		LOG.debug("=setUp()=");
 		LOG.debug("*********************");
 		
-		/*
-		nboardList = Arrays.asList(
-				 new NBoardVO(1,"J01_제목_124",0,"J01_내용","jamesol01@paran.com","NO_DATE")
-				,new BoardVO(2,"J02_제목_124",0,"J02_내용","jamesol02@paran.com","NO_DATE")					
-				,new BoardVO(3,"J03_제목_124",0,"J03_내용","jamesol03@paran.com","NO_DATE")
+		boardList = Arrays.asList(
+				new NBoardVO(1,"게시글말머리01","게시글제목01","J01_내요오오오옹","","","",0,"작성자01","","20200510","")
+				,new NBoardVO(2,"게시글말머리02","게시글제목02","J02_내요오오오옹","","","",0,"작성자02","","20200510","")					
+				,new NBoardVO(3,"게시글말머리03","게시글제목03","J03_내요오오오옹","","","",0,"작성자03","","20200510","")
 				);
-				*/
 				
 		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 		LOG.debug("=====================");
@@ -89,15 +78,45 @@ public class TestNBoardController {
 
 	}
 	
+	//목록조회
 	@Test
 	public void doRetrieve()  throws Exception {
 		//1.전체삭제
 		//2.3건입력
 		//3.조회
-	
-		
+		dao.doDeleteAll();
+		int flag = dao.doInsert(boardList.get(0));
+		flag += dao.doInsert(boardList.get(1));
+		flag += dao.doInsert(boardList.get(2));
+
+		assertThat(flag, is(3));
+
+		//url+param
+		MockHttpServletRequestBuilder  createMesage 
+					= MockMvcRequestBuilders.get("/nboard/do_retrieve.do")
+					.param("pageNum", "1")
+					.param("pageSize", "10")
+					.param("searchDiv", "")
+					.param("searchWord", " ");
+		;
+
+		ResultActions  resultActions = mockMvc.perform(createMesage)
+				.andExpect(status().is2xxSuccessful())	
+				.andExpect(model().attributeExists("searchList"))
+				.andExpect(model().attributeExists("list"))
+				.andExpect(model().attributeExists("vo"))
+				.andExpect(model().attributeExists("totalCnt"))
+				;
+
+		String result = resultActions.andDo(print())
+				.andReturn()
+				.getResponse().getContentAsString();
+		LOG.debug("=====================");
+		LOG.debug("=result="+result);
+		LOG.debug("=====================");  				
 	}
 	
+	//수정
 	@Test
 	@Ignore
 	public void doUpdate() throws Exception {
@@ -108,9 +127,51 @@ public class TestNBoardController {
 		//4.수정
 		//5.수정데이터 조회
 		//6.비교 
+		dao.doDeleteAll();
+
+		int flag = dao.doInsert(boardList.get(0));
+		assertThat(flag, is(1));
+
+		NBoardVO boardId = (NBoardVO) dao.doSelectOneTitle(boardList.get(0));
+		LOG.debug("======doUpdate()===============");
+		LOG.debug("=NBoardVO="+boardId);
+		LOG.debug("=====================");  
+
+		boardId.setNbCategory("update");
+		boardId.setNbTitle(boardId.getNbTitle()+"_U");
+		boardId.setNbContents(boardId.getNbContents()+"_U");
+		boardId.setRegId("admin_U");
 		
+
+		//url+param
+		MockHttpServletRequestBuilder  createMesage 
+		           = MockMvcRequestBuilders.post ("/nboard/do_update.do")
+		           	 .param("nbNo", boardId.getNbNo()+"")
+		           	 .param("nbCategory", boardId.getNbCategory()+"")
+		             .param("nbTitle", boardId.getNbTitle()+"")
+					 .param("nbContents", boardId.getNbContents())
+					 .param("regId", boardId.getRegId())
+					;
+		ResultActions  resultActions = mockMvc.perform(createMesage)
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8))
+			    .andExpect(MockMvcResultMatchers.jsonPath("$.msgId", is("1")));
+				;
+
+				String result = resultActions.andDo(print())
+						.andReturn()
+						.getResponse().getContentAsString();
+		LOG.debug("=====================");
+		LOG.debug("=result="+result);
+		LOG.debug("=====================");  		
+	
+		//5.수정데이터 조회
+		NBoardVO vsVO = (NBoardVO) dao.doSelectOne(boardId);
+		//6.비교 
+//		checkSameUser(vsVO,boardId);
 	}
 	
+	//단건삭제
 	@Test
 	@Ignore
 	public void doDelete() throws Exception {
@@ -118,18 +179,66 @@ public class TestNBoardController {
 		//2.단건입력
 		//3.단건조회:board_id조회
 		//4.삭제
-		  		
-				
+		dao.doDeleteAll();
+
+		int flag = dao.doInsert(boardList.get(0));
+		assertThat(flag, is(1));
+		
+		NBoardVO boardId = (NBoardVO) dao.doSelectOneTitle(boardList.get(0));
+		LOG.debug("======doDelete()===============");
+		LOG.debug("=BoardVO="+boardId);
+		LOG.debug("=====================");
+		
+		//url+param
+		MockHttpServletRequestBuilder  createMesage 
+				= MockMvcRequestBuilders.post ("/nboard/do_delete.do")
+				.param("nbNo", boardId.getNbNo()+"");		
+
+		ResultActions  resultActions = mockMvc.perform(createMesage)
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.msgId", is("1")));
+		;
+
+		String result = resultActions.andDo(print())
+				.andReturn()
+				.getResponse().getContentAsString();
+		LOG.debug("=====================");
+		LOG.debug("=result="+result);
+		LOG.debug("=====================");		
 	}
 	
 	//단건조회
 	@Test
 	@Ignore
 	public void doSelectOne() throws Exception {
-		 		
+		dao.doDeleteAll();
+		
+		int flag = dao.doInsert(boardList.get(0));
+		assertThat(flag, is(1));
+		
+		NBoardVO nbNo = (NBoardVO) dao.doSelectOneTitle(boardList.get(0));
+		LOG.debug("======doSelectOne()===============");
+		LOG.debug("=NBoardVO="+nbNo);
+		LOG.debug("=====================");
+		
+		//url+param
+		MockHttpServletRequestBuilder  createMesage 
+				= MockMvcRequestBuilders.get("/nboard/do_selectone.do")
+		.param("nbTitle", nbNo.getNbTitle());		
 
+		ResultActions  resultActions = mockMvc.perform(createMesage)
+				.andExpect(status().is2xxSuccessful())	
+				;
+		String result = resultActions.andDo(print())
+				.andReturn()
+				.getResponse().getContentAsString();
+		LOG.debug("=====================");
+		LOG.debug("=result="+result);
+		LOG.debug("====================="); 
 	}
 	
+	//등록
 	@Test
 	@Ignore
 	public void add() throws Exception {
@@ -137,21 +246,21 @@ public class TestNBoardController {
 		
 		MockHttpServletRequestBuilder createMessage = 
 				MockMvcRequestBuilders.post("/nboard/do_insert.do")
+				.param("nbCategory", boardList.get(0).getNbCategory())
 				.param("nbTitle", boardList.get(0).getNbTitle())
 				.param("nbContents", boardList.get(0).getNbContents())
 				.param("regId", boardList.get(0).getRegId())
 				;
-		
 		ResultActions  resultActions =mockMvc.perform(createMessage)
-				       .andExpect(MockMvcResultMatchers.content().contentType("application/json;charset=UTF-8"))
-				       .andExpect(MockMvcResultMatchers.jsonPath("$.msgId", is("1")))
-				;
+			       .andExpect(MockMvcResultMatchers.content().contentType("application/json;charset=UTF-8"))
+			       .andExpect(MockMvcResultMatchers.jsonPath("$.msgId", is("1")))
+			;
 		String result = resultActions.andDo(print())
-						.andReturn()
-						.getResponse().getContentAsString();
-		LOG.debug("=====================");
+				.andReturn()
+				.getResponse().getContentAsString();
+		LOG.debug("========add()=============");
 		LOG.debug("=result="+result);
-		LOG.debug("=====================");  
+		LOG.debug("=====================");
 		
 		NBoardVO titleVO = (NBoardVO) dao.doSelectOneTitle(boardList.get(0));
 		NBoardVO vsVO = (NBoardVO) dao.doSelectOne(titleVO);
@@ -160,6 +269,7 @@ public class TestNBoardController {
 	
 	public void checkSameUser(NBoardVO orgVO, NBoardVO vsVO) {
 		//assertTrue(orgVO.equals(vsVO));
+		assertThat(orgVO.getNbCategory(), is(vsVO.getNbCategory()));
 		assertThat(orgVO.getNbTitle(), is(vsVO.getNbTitle()));
 		assertThat(orgVO.getNbContents(), is(vsVO.getNbContents()));
 		assertThat(orgVO.getRegId(), is(vsVO.getRegId()));
@@ -180,8 +290,6 @@ public class TestNBoardController {
 		assertThat(1, is(1));
 	
 	}
-	
-	
 	
 
 	
