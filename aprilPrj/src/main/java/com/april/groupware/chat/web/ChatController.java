b59bd152db6d5e889d2c06177e04a144352decc9
sist <sist@DESKTOP-499LE1F>
2020-05-23 17:13:24 +0900

    채팅사원목록조회수정_정수연_20200523

--- aprilPrj/src/main/java/com/april/groupware/chat/web/ChatController.java
+++ aprilPrj/src/main/java/com/april/groupware/chat/web/ChatController.java
@@ -1,5 +1,7 @@
 package com.april.groupware.chat.web;
 
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
@@ -10,11 +12,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.april.groupware.chat.service.ChatService;
 import com.april.groupware.chat.service.ChatVO;
 import com.april.groupware.cmn.SearchVO;
 import com.april.groupware.cmn.StringUtil;
+import com.april.groupware.code.service.CodeService;
+import com.april.groupware.code.service.CodeVO;
 
 @Controller
 public class ChatController {
@@ -23,6 +28,27 @@ public class ChatController {
 	
 	@Autowired
 	ChatService chatService;
+	
+	@Autowired
+	CodeService codeService;
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/chat.do", method = RequestMethod.GET)
+	public String chat(Locale locale, Model model) {
+		LOG.debug("~환영~ 채팅방 좀 열려라 시발 ~ {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+	
+		String formattedDate = dateFormat.format(date);
+		model.addAttribute("serverTime", formattedDate);
+	
+		return "views/chatchat";
+	}
+	
+	
 	
 	@RequestMapping(value = "chat/do_retrieve.do",method = RequestMethod.GET)
 	public String doRetrieve(SearchVO search, Model model) {
@@ -35,21 +61,27 @@ public class ChatController {
 		}
 		
 		search.setSearchDiv(StringUtil.nvl(search.getSearchDiv()));
-		search.setSearchWord(StringUtil.nvl(search.getSearchWord().trim()));
+		search.setSearchWord(StringUtil.nvl(search .getSearchWord().trim()));
 
 		LOG.debug("1=================");
 		LOG.debug("1=param="+search);
 		LOG.debug("1=================");
-		
-		//페이지 사이즈: PAGE_SIZE
-		//화면에서 페이즈 사이즈 받아와서 searchVO에 넣어주고
-		//값이없으면 default값 하나 지정해두고
-		//지정된 버튼 가저오고
-
 		//검색조건 화면으로 전달.
 		model.addAttribute("vo", search);
+		
 		//TODO: codeTable : 검색조건,페이지 사이즈
-
+		//검색조건
+		CodeVO code=new CodeVO();
+		code.setCodeTypeId("CHAT_SEARCH");
+		List<CodeVO> searchList=(List<CodeVO>) this.codeService.doRetrieve(code);
+		LOG.debug("1.1=searchList="+searchList);
+		model.addAttribute("searchList", searchList);
+		
+		//페이지 사이즈: PAGE_SIZE
+		code.setCodeTypeId("PAGE_SIZE");
+		List<CodeVO> pageSizeList=(List<CodeVO>) this.codeService.doRetrieve(code);
+		LOG.debug("1.1=pageSizeList="+pageSizeList);
+		model.addAttribute("pageSizeList", pageSizeList);
 		
 		List<ChatVO> list = (List<ChatVO>) this.chatService.doRetrieve(search);
 		//조회결과 화면 전달
@@ -69,26 +101,21 @@ public class ChatController {
 		LOG.debug("1.2=================");
 		//조회결과 화면 전달
 		model.addAttribute("totalCnt", totalCnt);
-		return "chat/user_list";// /board/board_list.jsp
+		return "views/chat_list";// /board/board_list.jsp
 	
-		
-		
 	}
 	
 	
 	
 	@RequestMapping(value = "chat/do_selectone.do",method = RequestMethod.GET)
 	public String doSelectOne(ChatVO user,Locale locale,Model model) {
-	
+
 		//param board_id
 		LOG.debug("1=================");
 		LOG.debug("1=param="+user);
 		LOG.debug("1=================");
 		
-//		user.setId("kim56");
-		
 //		if( null == user.getId()) {		
-//
 //			throw new IllegalArgumentException("사용자 ID를 확인 하세요.");			
 //		}
 		
