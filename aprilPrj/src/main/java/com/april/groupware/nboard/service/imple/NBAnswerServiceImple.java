7dad2a0777bd2f9594a274b9c7efe1d5f019f72b
sist <sist@DESKTOP-IIRC78O>
2020-05-24 19:04:19 +0900

    게시판 댓글 Service Test완료_양은영_20200524

---
+++ aprilPrj/src/main/java/com/april/groupware/nboard/service/imple/NBAnswerServiceImple.java
@@ -0,0 +1,54 @@
+package com.april.groupware.nboard.service.imple;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.april.groupware.cmn.DTO;
+import com.april.groupware.nboard.service.NBAnswerDao;
+import com.april.groupware.nboard.service.NBAnswerService;
+
+@Service
+public class NBAnswerServiceImple implements NBAnswerService {
+	private final Logger  LOG = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	NBAnswerDao  dao;
+	
+	@Override
+	public int doInsert(DTO dto) {
+		return dao.doInsert(dto);
+	}
+
+	@Override
+	public int doUpdate(DTO dto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public DTO doSelectOne(DTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int doDelete(DTO dto) {
+		return dao.doDelete(dto);
+	}
+
+	@Override
+	public List<?> doRetrieve(DTO dto) {
+		return dao.doRetrieve(dto);
+	}
+
+	@Override
+	public List<?> getAll(DTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
